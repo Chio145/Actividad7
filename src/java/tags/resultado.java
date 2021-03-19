b87a03e01eb079938295581a89aabc3944d95176
Chio145 <rocio.aguirre.ba@gmail.com>
2021-03-18 23:20:03 -0600

    Act6 Java

---
+++ src/java/tags/resultado.java
@@ -0,0 +1,27 @@
+
+package tags;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.JspFragment;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+/**
+ *
+ * @author arete
+ */
+public class resultado extends SimpleTagSupport {
+    StringWriter name = new StringWriter();
+
+    @Override
+    public void doTag() throws JspException, IOException{
+    
+       
+        getJspBody().invoke(name);
+        getJspContext().getOut().println("Soy "+name.toString()+ "Fecha: 18/03/2021");      
+        JspFragment f = getJspBody();
+    }
+    
+}
---
+++ web/index.jsp
@@ -0,0 +1,21 @@
+<%-- 
+    Document   : index
+    Created on : 18 mar 2021, 22:06:45
+    Author     : arete
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="tags" uri="/WEB-INF/tlds/biblioteca_tag" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <tags:mostrarNombre></tags:mostrarNombre>
+        <tags:resultado>  Rocio </tags:resultado>
+            
+    </body>
+</html>
